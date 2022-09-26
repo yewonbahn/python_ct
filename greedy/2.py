af28ce2ab5b386e5732c63e8848990843b4094f2
Yewon Bahn <banya@Yewonui-MacBookAir.local>
2022-09-26 16:23:39 +0900

    [1주차] 0926

--- greedy/2.py
+++ greedy/2.py
@@ -1,1 +1,20 @@
 # 큰수의 법칙
+
+n,m,k = map(int,input().split())
+lst = list(map(int, input().split()))
+
+lst.sort()
+lst.reverse()
+cnt=0
+answer=0
+
+for i in range(m):
+    if(k==cnt):
+        answer += lst[1]
+        cnt=0
+    else:
+        answer+=lst[0]
+        cnt+=1;
+
+
+print(answer)
