aebceb76b68882362f395371328dc7ac3805fa23
Yewon Bahn <banya@Yewonui-MacBookAir.local>
2022-09-26 16:32:29 +0900

    [1주차] 0926

--- greedy/2.py
+++ greedy/2.py
@@ -1,20 +1,24 @@
 # 큰수의 법칙
-
+#가장 큰수 K번 더하고 두번째로 큰수 한번 더하는 연산
 n,m,k = map(int,input().split())
 lst = list(map(int, input().split()))
 
 lst.sort()
 lst.reverse()
-cnt=0
 answer=0
+#
+# for i in range(m):
+#     if(k==cnt):
+#         answer += lst[1]
+#         cnt=0
+#     else:
+#         answer+=lst[0]
+#         cnt+=1;
 
-for i in range(m):
-    if(k==cnt):
-        answer += lst[1]
-        cnt=0
-    else:
-        answer+=lst[0]
-        cnt+=1;
+# M의 크기가 100억 이상이라면?
+cnt=m/(k+1)*k+m%(k+1)
+answer+=cnt*lst[0]
+answer+=(m-cnt)*lst[1]
 
 
-print(answer)
+print(int(answer))
