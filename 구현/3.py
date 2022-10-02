2713b5ba5869881a47437751f146a5d5790aef67
Yewon Bahn <banya@Yewonui-MacBookAir.local>
2022-10-02 18:32:31 +0900

    1002 -ing

--- 구현/3.py
+++ 구현/3.py
@@ -19,6 +19,20 @@ def turn_left():
         direction = 3
 count =1
 turn_time=0
+# 1 1 0 예시
+# 1: direction = 3(북쪽을 바라 보고,왼쪽 방향), turn_time =1
+# 2: direction = 2(북,아래 뱡향), 2 1, tutn_time=2
+# 3: direction = 1(북,오른쪽 뱡향), 1 2, tutn_time=2
+#   if 문 들어감 -> d[1][2] = 1, x=1,y=2 count=2,turn_time=0
+# 4: direction = 0,(동, 윗부터) 0,2. turn_time=1
+# 5: direction = 3,(동,왼쪽) 1,1 이미갔기때문에  turn_time=2
+# 6: direction = 2,(동,아래 ) 2, 2 d[2][2]=1,
+#    x=2,y=2, count=3, turn_time=0
+# 7 :direction =1 (남,오른쪽) 2,3 turn_time=1
+# 8: direction=0 (남,위) 1,2 turn_time=2
+# 9: direction=3 (남,왼쪽) 2,1 turn_time=3
+#10 : d=2 (남,아래)3,2 turn_time=4=> 1,2
+#11:
 while True:
     turn_left()
     nx=x+dx[direction]
