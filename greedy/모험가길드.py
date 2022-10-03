c8e54f7ee732d7b1cfd1685830f5be9178e3e49c
yewonbahn <banyaewon98@gmail.com>
2022-10-04 01:58:40 +0900

    1004 -ing

--- greedy/모험가길드.py
+++ greedy/모험가길드.py
@@ -3,15 +3,13 @@ a = int(input())
 team=list(map(int,input().split()))
 
 team.sort()
-team.reverse()
-idx=0
-cnt=0
-for i in range(len(team)):
 
-    max = team[i]
-    i+=max
-    idx += max
-    if (idx > len(team)):
-        print(cnt)
-        break
-    cnt+=1
+cnt=0 # 총그룹수
+mb = 0 #현재 그룹에 포함된 모헙가 수
+
+for i in team:
+    mb+=1
+    if mb >= i:
+        cnt+=1
+        mb = 0
+print(cnt)
