7d5797a4915d4840a580ca6de0c2d866b14cccae
Yewon Bahn <banya@Yewonui-MacBookAir.local>
2022-10-21 00:47:27 +0900

    1021 -ing

---
+++ dfs,bfs/2178.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+n,m=map(int,input().split())
+array=[]
+for i in range(n):
+    array.append(list(map(int, list(input()))))
+
+print(array)
+dx=[-1,1,0,0]
+dy=[0,0,1,-1]
+visited=[[False]*m for i in range(n)]
+print(visited)
+x,y=0,0
+q=deque()
+cnt=0
+q.append((x,y))
+while (x,y)!=(n-1,m-1):
+
+    x,y=q.popleft()
+
+    if not visited[x][y]:
+        print(x, y)
+        visited[x][y] = True
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m or array[nx][ny]==0:
+                cnt-=1
+                continue
+            else:
+                cnt+=1
+                q.append((nx,ny))
+print(cnt)
