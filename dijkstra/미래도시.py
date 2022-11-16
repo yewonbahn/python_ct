eaadd3becba43b675fb81d4b6bce1c63c3ba47a5
yewonbahn <banyaewon98@gmail.com>
2022-11-16 15:48:05 +0900

    문제 풀기

---
+++ dijkstra/미래도시.py
@@ -0,0 +1,29 @@
+INF = int(1e9)
+n,m=map(int,input().split())
+
+graph=[[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        if a==b:
+            graph[a][b]=0
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a][b]=1
+    graph[b][a]=1
+x,k=map(int,input().split())
+
+
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b]=min(graph[a][b],graph[a][k]+graph[k][b])
+            # graph[b][a]=min(graph[b][a],graph[b][k]+graph[k][a])
+            #
+
+dis=(graph[1][k]+graph[k][x])
+if dis==INF:
+    print("-1")
+else:
+    print(dis)
