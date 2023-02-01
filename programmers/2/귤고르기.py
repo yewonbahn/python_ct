938181eeb042b93c4de059d73315291a6e396724
yewonbahn <banyaewon98@gmail.com>
2023-02-01 22:54:55 +0900

    [lv2] 귤고르기

---
+++ programmers/2/귤고르기.py
@@ -0,0 +1,20 @@
+def solution(k, tangerine):
+    counter = {}
+    answer = 0
+    for i in tangerine:
+        counter.setdefault(i, 0)
+        counter[i] += 1
+
+    value = len(tangerine) - k
+
+    cnt=0
+    counter = sorted(counter.items(),key=lambda x: x[1])
+    for k,v in counter:
+        if value>=v:
+            value -= v
+            cnt+=1
+        else:
+            break
+
+
+    return len(counter)-cnt
