1cc5a773bfbb070dfa1af9844f9955db71caabb9
yewonbahn <banyaewon98@gmail.com>
2023-04-13 17:17:36 +0900

    0413 programmers lv2 solved

--- dp/2.py
+++ dp/2.py
@@ -3,13 +3,17 @@ a=int(input())
 d=[0]*30001
 
 for i in range(2,a+1):
+    print(i,"일떄")
     d[i]=d[i-1]+1
     if i%2==0:
+        print("1")
         d[i]=min(d[i],d[i//2]+1)
     if i%3==0:
+        print("2")
         d[i]=min(d[i],d[i//3]+1)
     if i%5==0:
+        print("3")
         d[i]=min(d[i],d[i//5]+1)
-
+print(d)
 print(d[a])
 
